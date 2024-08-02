39f1f189362ce46f94cc34e88b0632b9678c168c
Anshu Sharma <148671722+SharmAnshu5@users.noreply.github.com>
2024-08-02 12:32:58 +0530

    Pattern_Right_Pascal_Triangle.java

--- Pattern_Right_Pascal_Triangle.java
+++ Pattern_Right_Pascal_Triangle.java
@@ -6,7 +6,7 @@ public class Pattern_Right_Pascal_Triangle {
         System.out.println("Enter the number of rows: ");
         int rows = scanner.nextInt();
 
-        // Print the upper half of the Pascal's triangle
+       
         for (int i = 0; i <= rows - 1; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*" + " ");
@@ -14,7 +14,7 @@ public class Pattern_Right_Pascal_Triangle {
             System.out.println("");
         }
 
-        // Print the lower half of the Pascal's triangle
+        
         for (int i = rows - 1; i >= 0; i--) {
             for (int j = 0; j <= i - 1; j++) {
                 System.out.print("*" + " ");
