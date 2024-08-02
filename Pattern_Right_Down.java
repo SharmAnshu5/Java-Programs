ee10d7510118973bc10aeb3e6caab9e3fbb751fa
Anshu Sharma <148671722+SharmAnshu5@users.noreply.github.com>
2024-08-02 12:32:15 +0530

    Pattern_Right_Down.java

--- Pattern_Right_Down.java
+++ Pattern_Right_Down.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Pattern_Right_Down {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // takes input
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows: ");
         int rows = sc.nextInt();
 
