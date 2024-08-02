4a3be80cc0839d4a3873a300a7153bdbd3cbb342
Anshu Sharma <148671722+SharmAnshu5@users.noreply.github.com>
2024-08-02 12:20:45 +0530

    Pattern_Diamond_Alphabets.java

--- Pattern_Diamond_Alphabets.java
+++ Pattern_Diamond_Alphabets.java
@@ -5,7 +5,7 @@ public class Pattern_Diamond_Alphabets {
         char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
                 'W', 'X', 'Y', 'Z' };
-        int letterNumber = 0;
+        int letter = 0;
         String[] diamond = new String[26]; // array of strings
 
         System.out.print("Enter a Character between A to Z: ");
@@ -14,26 +14,25 @@ public class Pattern_Diamond_Alphabets {
         try {
             char userLetter = scanner.next("[A-Z]").charAt(0);
 
-            // Find the position of the user's letter in the alphabet
             for (int i = 0; i < letters.length; i++) {
                 if (letters[i] == userLetter) {
-                    letterNumber = i;
+                    letter = i;
                     break;
                 }
             }
 
-            // Construct the diamond pattern
-            for (int i = 0; i <= letterNumber; i++) {
+        
+            for (int i = 0; i <= letter; i++) {
                 diamond[i] = "";
-                // Add leading spaces
-                for (int j = 0; j < letterNumber - i; j++) {
+            
+                for (int j = 0; j < letter - i; j++) {
                     diamond[i] += " ";
                 }
 
-                // Add the letter
+        
                 diamond[i] += letters[i];
 
-                // Add spaces between letters (if not the first letter)
+    
                 if (letters[i] != 'A') {
                     for (int j = 0; j < 2 * i - 1; j++) {
                         diamond[i] += " ";
@@ -41,12 +40,11 @@ public class Pattern_Diamond_Alphabets {
                     diamond[i] += letters[i];
                 }
 
-                // Print the row
+        
                 System.out.println(diamond[i]);
             }
 
-            // Print the second half of the diamond (in reverse order)
-            for (int i = letterNumber - 1; i >= 0; i--) {
+            for (int i = letter - 1; i >= 0; i--) {
                 System.out.println(diamond[i]);
             }
         } catch (Exception e) {
