f65250b71354493c5cabcba69152c9cbbef27c27
Anshu Sharma <148671722+SharmAnshu5@users.noreply.github.com>
2024-08-02 12:31:22 +0530

    Pattern_Left_Pascal_Triangle.java

--- Pattern_Left_Pascal_Triangle.java
+++ Pattern_Left_Pascal_Triangle.java
@@ -6,26 +6,26 @@ public class Pattern_Left_Pascal_Triangle {
         System.out.println("Enter the number of rows: ");
         int rows = scanner.nextInt();
 
-        // Print the upper half of the left-aligned Pascal's triangle
+       
         for (int i = 1; i <= rows; i++) {
-            // Print leading spaces
+           
             for (int j = i; j < rows; j++) {
                 System.out.print(" ");
             }
-            // Print asterisks
+           
             for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
 
-        // Print the lower half of the left-aligned Pascal's triangle
+       
         for (int i = rows; i >= 1; i--) {
-            // Print leading spaces
+            
             for (int j = i; j <= rows; j++) {
                 System.out.print(" ");
             }
-            // Print asterisks
+           
             for (int k = 1; k < i; k++) {
                 System.out.print("*");
             }
